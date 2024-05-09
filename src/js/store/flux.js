6f2c5226c97fbcc3531ce710adb5a6c367dbf71a
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-05-09 11:38:11 +0000

    correccion errores

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,63 +12,68 @@ const getState = ({ getStore, getActions, setStore }) => {
 					createUser();
 				}
 				return resp.json()
-			}).then(user => setUser(user))
-				
-			},
-			createAgenda: async () => {
-				await fetch("https://playground.4geeks.com/contact/agendas/mariasr79",
-					{ method: "POST" }
-				);
+			}
+
+		},
+		createAgenda: async () => {
+			await fetch("https://playground.4geeks.com/contact/agendas/mariasr79",
+				{ method: "POST" }
+			);
 
 
-			},
+		},
 
-			createContact: async (name, phone, email, address) => {
-				await fetch("https://playground.4geeks.com/contact/agendas/mariasr79/contacts",
-					{method: "POST",
-						body: JSON.stringify({
-							"name": name,
-							"phone": phone,
-							"email": email,
-							"address": address
-						  })
-					}
-				)
-			// 	.then(resp => {
-			// 		if (resp.ok) {
-			// 			return resp.json()
-			// 		}
-			// 	}).then(respJson => {
-			// 		const userTaks = user.todos;
-			// 		const newUser = {
-			// 			...user,
-			// 			todos: [...userTaks, respJson]
-			// 		};
-			// },
-			editContact: async (name, phone, email, address, contactid) => {
-				await fetch(`https://playground.4geeks.com/contact/agendas/mariasr79/contacts/${contactid}`,
-					{method: "PUT",
-						body: JSON.stringify({
-							"name": name,
-							"phone": phone,
-							"email": email,
-							"address": address
-						  })
-					}
-				)
-				// const validateTask = (task) => {
-				// 	if (!task || !task.trim()) {
-				// 		alert("el valor de la tarea no puede ser vacio")
-				// 	}
-				// 	createTask(task)
-				// 	setTask("");
-				// }
-			},
-			deleteContact: async (contactid) => {
-				await fetch(`https://playground.4geeks.com/contact/agendas/mariasr79/contacts/${contactid}`,
-					{method: "DELETE"
-						
-					}
+		createContact: async (name, phone, email, address) => {
+			await fetch("https://playground.4geeks.com/contact/agendas/mariasr79/contacts",
+				{
+					method: "POST",
+					body: JSON.stringify({
+						"name": name,
+						"phone": phone,
+						"email": email,
+						"address": address
+					})
+				}
+			)
+
+		},
+		// 	.then(resp => {
+		// 		if (resp.ok) {
+		// 			return resp.json()
+		// 		}
+		// 	}).then(respJson => {
+		// 		const userTaks = user.todos;
+		// 		const newUser = {
+		// 			...user,
+		// 			todos: [...userTaks, respJson]
+		// 		};
+		// },
+		editContact: async (name, phone, email, address, contactid) => {
+			await fetch(`https://playground.4geeks.com/contact/agendas/mariasr79/contacts/${contactid}`,
+				{
+					method: "PUT",
+					body: JSON.stringify({
+						"name": name,
+						"phone": phone,
+						"email": email,
+						"address": address
+					})
+				}
+			)
+			// const validateTask = (task) => {
+			// 	if (!task || !task.trim()) {
+			// 		alert("el valor de la tarea no puede ser vacio")
+			// 	}
+			// 	createTask(task)
+			// 	setTask("");
+			// }
+		},
+		deleteContact: async (contactid) => {
+			await fetch(`https://playground.4geeks.com/contact/agendas/mariasr79/contacts/${contactid}`,
+				{
+					method: "DELETE"
+
+				}
 				// }).then(resp => {
 				// 	if (resp.ok) {
 				// 		const userTaks = user.todos.filter(item => item.id !== task.id)
@@ -77,17 +82,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 				// 			todos: [...userTaks]
 				// 		};
 				// 		setUser(newUser)
-				// )
-			}
-
-
-		},
-
-
-
+			)
+		}
 
 	}
-};
-
-
+}
+	
 export default getState;
